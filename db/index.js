d8ef566b1e89f434a42ee7d9329fed55b2520b01
tanner <tannerlessi@gmail.com>
2022-10-27 10:06:46 -0700

    about to test code

--- db/index.js
+++ db/index.js
@@ -77,6 +77,78 @@ async function createTags(tagList) {
   }
 }
 
+async function createPostTag(postId, tagId) {
+  try {
+    await client.query(
+      `
+  INSERT INTO post_tags("postId", "tagId")
+  VALUES ($1, $2)
+  ON CONFLICT ("postId", "tagId") DO NOTHING;
+`,
+      [postId, tagId]
+    );
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function addTagsToPost(postId, tagList) {
+  try {
+    const createPostTagPromises = tagList.map((tag) =>
+      createPostTag(postId, tag.id)
+    );
+
+    await Promise.all(createPostTagPromises);
+    return await getUserById(postId);
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getPostById(postId) {
+  try {
+    const {
+      rows: [post],
+    } = await client.query(
+      `
+    SELECT *
+    FROM posts
+    WHERE id=$1;
+    `,
+      [postId]
+    );
+    const { rows: tags } = await client.query(
+      `
+    SELECT tags.*
+    FROM tags
+    JOIN post_tags ON tags.id=post_tags."tagId"
+    WHERE post_tags."postId"=$1;
+  `,
+      [postId]
+    );
+
+    const {
+      rows: [author],
+    } = await client.query(
+      `
+    SELECT id, username, name, location
+    FROM users
+    WHERE id=$1;
+  `,
+      [post.authorId]
+    );
+
+    post.tags = tags;
+    post.author = author;
+
+    delete post.authorId;
+
+    return post;
+  } catch (error) {
+    throw error;
+  }
+}
+
 async function updateUser(id, fields = {}) {
   // build the set string
   console.log("id", id);
@@ -208,4 +280,7 @@ module.exports = {
   getPostsByUser,
   getUserById,
   createPost,
+  getPostById,
+  addTagsToPost,
+  createTags,
 };
